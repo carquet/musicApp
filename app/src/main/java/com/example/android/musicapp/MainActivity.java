d7debe70407a2cf24be2bcd261d079e73d4fbb30
L.Carquet <laetitiacarquet@gmail.com>
2018-04-24 13:45:00 +0200

    adds content in suitup activity and displays it in suit up layout

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView suitUpButton = (TextView) findViewById(R.id.suit_up_view);
+        suitUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent suitUpIntent = new Intent(MainActivity.this, SuitUpActivity.class);
+                startActivity(suitUpIntent);
+            }
+        });
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/android/musicapp/SuitUpActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.musicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SuitUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suit_up);
+
+        //create content - put it in a an ArraysList
+        ArrayList<String> suitUpSongs = new ArrayList<>();
+        suitUpSongs.add("It's been a hard day work");
+        suitUpSongs.add("Sonata piano");
+        suitUpSongs.add("Hoy no me quiero levantar");
+        suitUpSongs.add("I am no Superman");
+        suitUpSongs.add("Le travail c'est la santé");
+        suitUpSongs.add("Who's the boss");
+        suitUpSongs.add("Confident");
+
+        //grab the root view
+        LinearLayout root = (LinearLayout) findViewById(R.id.rootView);
+
+        //populate it with the content you created
+        for(int index = 0; index < suitUpSongs.size();index++){
+            TextView albumView = new TextView(this);
+            albumView.setText(suitUpSongs.get(index));
+            root.addView(albumView);
+
+        }
+    }
+}
