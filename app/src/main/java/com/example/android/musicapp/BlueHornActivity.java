a4118ba9cb8f28cf0c462714a79e4652bf18a5c6
L.Carquet <laetitiacarquet@gmail.com>
2018-04-28 13:39:44 +0200

    adds a on item click listener to Bluehorn Activity and adds a now playing acitivty

--- app/src/main/java/com/example/android/musicapp/BlueHornActivity.java
+++ app/src/main/java/com/example/android/musicapp/BlueHornActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -31,5 +34,29 @@ public class BlueHornActivity extends AppCompatActivity {
 
         //3. plug the adapter onto the view
         songListView.setAdapter(itemsAdapter);
+
+        //Set item click listener on each item
+        /*
+        AdapterView: The AdapterView where the click happened
+        View: The view within the AdapterView that was clicked (this will be a view provided by the adapter)
+        int: The position of the view in the adapter.
+        long: The row id of the item that was clicked.
+         */
+        songListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view,
+                                    int position, long id) {
+
+                Song currentSong = songsList.get(position);
+                String artist = currentSong.getmArtist();
+                String title = currentSong.getmTitle();
+
+                Intent listViewIntent = new Intent(BlueHornActivity.this, NowPlayingActivity.class);
+                listViewIntent.putExtra("message", artist);
+                listViewIntent.putExtra("message2", title);
+                startActivity(listViewIntent);
+            }
+
+        });
     }
 }
